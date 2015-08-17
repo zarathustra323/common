1b3925c66632d6180c533ed917fa0d92d0d81bea
Jacob Bare <jacob.bare@cygnus.com>
2015-08-17 09:01:20 -0500

    Create String.php

---
+++ src/Zarathustra/Common/String.php
@@ -0,0 +1,50 @@
+<?php
+namespace Zarathustra/Common;
+
+class String
+{
+    /**
+     * Convert word into underscore format (e.g. some_name_here).
+     *
+     * @static
+     * @param   string  $word
+     * @return  string
+     */
+    public static function underscore($word)
+    {
+        return strtolower(preg_replace('~(?<=\\w)([A-Z])~', '_$1', $word));
+    }
+    /**
+     * Convert word into dasherized format (e.g. some-name-here).
+     *
+     * @static
+     * @param   string  $word
+     * @return  string
+     */
+    public static function dasherize($word)
+    {
+        return str_replace('_', '-', self::underscore($word));
+    }
+    /**
+     * Convert word into camelized format (e.g. someNameHere).
+     *
+     * @static
+     * @param   string  $word
+     * @return  string
+     */
+    public static function camelize($word)
+    {
+        return lcfirst(self::studlify($word));
+    }
+    /**
+     * Convert word into studly caps format (e.g. SomeNameHere).
+     *
+     * @static
+     * @param   string  $word
+     * @return  string
+     */
+    public static function studlify($word)
+    {
+        return str_replace(" ", "", ucwords(strtr(self::underscore($word), "_", " ")));
+    }
+}
